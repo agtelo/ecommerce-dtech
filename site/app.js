159b2382351ee706bec1a4b108a79f94fa7ee5ca
agtelo <alantelo1987agmail.com>
2020-12-28 00:32:31 -0300

    se agrega ruteo a login, contacto, producto, registro, carrito

--- site/app.js
+++ site/app.js
@@ -13,3 +13,29 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"))
 
 });
+
+app.get("login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+
+});
+
+app.get("contacto", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/contacto.html"))
+
+});
+
+app.get("producto", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/producto.html"))
+
+});
+
+app.get("registro", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/registro.html"))
+
+});
+
+app.get("carrito", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/carrito.html"))
+
+});
+
