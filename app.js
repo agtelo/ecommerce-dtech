82a1b0826ed8127d5b9e2894b682fbcd273a2936
agtelo <alantelo1987agmail.com>
2021-02-05 18:31:55 -0300

    se realizan ajustes en vista contacto y vista desarrollos

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 app.listen(process.env.PORT || 3000, function() {
