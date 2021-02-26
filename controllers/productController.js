34d4953c8abfec1058c8eeed2c0c420d55a2ba46
agtelo <alantelo1987agmail.com>
2021-02-26 05:49:37 -0300

    correciones en metodo guardar mainController

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,7 +15,7 @@ const productController = {
         return res.render('products/crear', { title: "Panel", css: "crud" });
     },
     guardar: function(req, res) {
-        let productoNuevo = {
+        let producto = {
             id: req.body.id,
             name: req.body.nombre,
             description: req.body.descripcion,
@@ -24,8 +24,17 @@ const productController = {
             price: req.body.precio,
             image: req.body.imagen
         };
-        let productoJSON = JSON.stringify(productoNuevo);
-        fs.writeFileSync( "./data/productoNuevo.json", productoJSON);
+        let archivoProducto = fs.readFileSync("./data/product.json", {encoding:"utf-8"});
+        let productos;
+        if (archivoProducto == ""){
+             productos = [];
+        } else {
+             productos = JSON.parse(archivoProducto);
+        }
+        productos.push(producto);
+
+        productoJSON = JSON.stringify(producto);
+        fs.appendFileSync( "./data/productoNuevo.json", productoJSON);
         res.redirect('./crear');
     },
 
