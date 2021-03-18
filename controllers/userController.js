9cd2494fd066888523269737e04f9c7769b47715
agtelo <alantelo1987agmail.com>
2021-03-18 00:22:38 -0300

    se crea ruta post en formulario users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+const path = require('path');
+let usuariosFilePath = path.resolve(__dirname, '../data/users.json');
+const usuarios = JSON.parse(fs.readFileSync(usuariosFilePath, 'utf-8'));
+
 const userController = {
     
     login: function(req,res){
@@ -7,6 +12,22 @@ const userController = {
     registro: function(req,res){
         return res.render ('users/registro', {title: "Registro", css: "login"});
     },
+    crearUsuario: function (req,res) {
+        let usuarioNuevo = {
+            username: req.body.usuario,
+            email: req.body.email,
+            firt_name: req.body.nombre,
+            last_name: req.body.apellido,
+            phone: req.body.telefono,
+            password: req.body.password,
+            image: req.file.filename
+            
+        };
+        usuarios.push(usuarioNuevo);
+        let usuarioSubir = JSON.stringify(usuarios, null , 2);
+		fs.writeFileSync(usuariosFilePath ,usuarioSubir)
+        res.render('users/usuario', {"usuarioNuevo" : usuarioNuevo});
+    },
 
     recuperoPass: function(req,res){
         return res.render ('users/recupero-pass', {title: "Recupero" ,css: "login,"} );
@@ -15,6 +36,9 @@ const userController = {
     admin: function(req,res) {
         return res.render ('/users/admin', {title: "Admin", css: "admin"});
     },
+    show: function (req,res) {
+        return res.render ('./users/usuario')
+    }
     
     
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,10 +4,24 @@ const router = express.Router();
 // la constante mainController requerirá el controlador
 const userController = require('../controllers/userController');
 
+const path = require("path");
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "./public/images/users");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))}
+});
+const uploadFIle = multer({ storage: storage});
+
 
 router.get('/login', userController.login);
 
 router.get('/registro', userController.registro);
+router.post('/registro',uploadFIle.single("imagen"), userController.crearUsuario);
+
+router.get('/usuario', userController.show);
 
 router.get('/recupero-pass', userController.recuperoPass);
 router.get('/admin', userController.admin);
