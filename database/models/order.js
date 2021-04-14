2a75e7547885f4e8ee15938e2ffc31b8b6a35797
agtelo <alantelo1987agmail.com>
2021-04-13 22:57:19 -0300

    correciones en modelos

--- database/models/order.js
+++ database/models/order.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Order.hasmany(models.Product, {
+      Order.hasMany(models.Product, {
         foreignKey: "product_id",
         as: "products"
       }),
