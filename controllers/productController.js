c98093a8ccf64e3efe6d48a3ea6cf8f921ebe589
agtelo <alantelo1987agmail.com>
2021-04-22 23:59:42 -0300

    cambios en product-panel

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,21 +5,6 @@ const productController = {
     producto: function(req, res) {
         db.Product.findAll()
             .then(function(products) {
-<<<<<<< HEAD
-                return res.render('products/productos', { title: "Articulo", css: "product-panel", "products" : products})
-            }
-                
-            )
-        
-    },
-    carrito: function(req, res) {
-        return res.render('products/carrito', { title: "Carrito", css: "carrito" });
-    },
-    show: function (req, res) {
-        return res.render('products/articulo', { title: "Articulo", css: "product-panel", "products" : products});
-    },
-    
-=======
                 return res.render('products/productos', { title: "Articulo", css: "product-panel", "products": products })
             })
     },
@@ -32,19 +17,13 @@ const productController = {
         return res.render('products/articulo', { title: "Articulo", css: "product-panel", "products": products });
     },
 
->>>>>>> af91d237ad705014c154eafd15bdd8ab2c560d88
     panel: function(req, res) {
-        return res.render('products/product-panel', { title: "Panel", css: "product-panel", "products" : products });
+        db.Product.findAll()
+            .then(function (products) {
+                return res.render('products/product-panel', { title: "Panel", css: "product-panel", "products" : products }); 
+            })
+        
     },
-<<<<<<< HEAD
-    crear: function(req, res) {
-        return res.render('products/crear', { title: "Panel", css: "crud"});
-    },
-    guardar: function(req, res) {
-        db.Product.create ({
-            name: req.body.nombre,
-            description: req.body.descripcion,
-=======
 
     crear(req, res) {
         db.Category.findAll()
@@ -90,7 +69,6 @@ const productController = {
             id: "",
             name: req.body.name,
             description: req.body.description,
->>>>>>> af91d237ad705014c154eafd15bdd8ab2c560d88
             item1: req.body.item1,
             item2: req.body.item2,
             item3: req.body.item3,
@@ -99,26 +77,12 @@ const productController = {
             item6: req.body.item6,
             item7: req.body.item7,
             item8: req.body.item8,
-<<<<<<< HEAD
-            category_id: req.body.categoria,
-            price: req.body.precio,
-=======
             category_id: req.body.category,
             price: req.body.price,
 
->>>>>>> af91d237ad705014c154eafd15bdd8ab2c560d88
             image: req.file.filename
         });
         res.redirect('./crear');
-<<<<<<< HEAD
-    },
-    editar: function(req, res) {
-        let productoAEditar = products.find( producto => producto.id == req.params.id);
-        return res.render("products/editar", {"productoAEditar" : productoAEditar});        
-    },
-    actualizar: function(req, res) {
-        const productoAEditar = products.find( producto => producto.id == req.params.id);
-=======
     },*/
 
     editar: function(req, res) {
@@ -142,7 +106,6 @@ const productController = {
     },
     actualizar: function(req, res) {
         /*const productoAEditar = products.find(producto => producto.id == req.params.id);
->>>>>>> af91d237ad705014c154eafd15bdd8ab2c560d88
 
 		const productoEditado = products.map( producto => {
 			if(producto.id == productoAEditar.id ){
@@ -166,26 +129,6 @@ const productController = {
 		let productoSubir = JSON.stringify(productoEditado, null , 2);
 		fs.writeFileSync(productsFilePath,productoSubir);
 
-<<<<<<< HEAD
-		res.redirect("/")
-    },
-    borrar: function(req, res) {
-        res.render()
-    },
-    destruir: function(req, res) {
-
-        let productoAEliminar = products.filter(producto => producto.id != req.params.id);
-            
-            /*let imagenABorrar = products.find( producto => producto.id == req.params.id);
-            let filePath = path.resolve(__dirname,'../public/img/products/' + imagenABorrar.image);
-            console.log(imagenABorrar);
-            console.log(filePath);
-            fs.unlinkSync(filePath);*/
-
-            let productoASubirJSON = JSON.stringify(productoAEliminar, null , 2);
-            fs.writeFileSync(productsFilePath,productoASubirJSON);
-            res.redirect("/")
-=======
         res.redirect("/")*/
     },
     destruir: function(req, res) {
@@ -198,7 +141,6 @@ const productController = {
             .then(() => {
                 res.redirect('/products/product-panel')
             })
->>>>>>> af91d237ad705014c154eafd15bdd8ab2c560d88
     }
 }
 
