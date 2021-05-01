b9e761b4a7d43bb473d37355e49b3e07c9338ec2
agtelo <alantelo1987agmail.com>
2021-05-01 01:36:17 -0300

    cambios en validateLogin

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function() {
 
     let formulario = document.querySelector("form.reservation");
 
-    formulario.addEventListener("submit", function(e) {
+    formulario.addEventListener("keyup", function(e) {
         let errores = [];
 
 
@@ -10,29 +10,43 @@ window.addEventListener("load", function() {
         
         if (campoEmail.value == "") {
             errores.push("Debes ingresar tu email");
+            campoEmail.classList.add("app-form-control-fail");
+            
+        } else {
+            campoEmail.classList.remove("app-form-control-fail");
+            campoEmail.classList.add("app-form-control");
         }
 
-        else if (campoEmail.value.length < 3) {
+        if (campoEmail.value.length < 3) {
             errores.push("El mail debe contener mas de 3 caracteres");
+            campoEmail.classList.add("app-form-control-fail");
         }
         
         let campoPassword = document.getElementById("pwd");
 
-        if (campoPassword.value == "") {
+        if (campoPassword.value == "") {    
             errores.push("Debes ingresar tu password");
+            campoPassword.classList.add("app-form-control-fail");
+        
         }
 
         if (campoPassword.value.length < 8) {
             errores.push("La contraseña debe al menos 8 caracteres");
-        }
+            campoPassword.classList.add("app-form-control-fail");
+            
+        } 
+
         if (!/[A-Z]/.test(campoPassword.value)) {
             errores.push("La contraseña debe tener al menos 1 mayúscula");
+            campoPassword.classList.add("app-form-control-fail");
         }
         if (!/[0-9]/.test(campoPassword.value)) {
             errores.push("La contraseña debe tener al menos 1 número");
+            campoPassword.classList.add("app-form-control-fail");
         }
         if (!/[!@#$%^&*]/.test(campoPassword.value)) {
             errores.push("La contraseña debe tener al menos 1 simbolo");
+            campoPassword.classList.add("app-form-control-fail");
         }
 
         if (errores.length > 0) {
@@ -41,13 +55,8 @@ window.addEventListener("load", function() {
             let ulErrores = document.querySelector("div.errores ul")
             for (let i = 0; i < errores.length; i++) {
                 ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
-
-
-
             }
-
         }
-
     })
 
 
